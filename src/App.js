9d4bbf11d741f4c0ca30d378821c474bb320b794
sasindudesshapriya <152970418+sasindudesshapriya@users.noreply.github.com>
2024-12-03 12:52:45 +0530

    Add all the files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import AddProperty from './pages/AddProperty';
+import EditProperty from './pages/EditProperty';
+import PropertyDetails from './components/PropertyDetails';
+import "./styles/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<AddProperty />} />
+        <Route path="/edit/:id" element={<EditProperty />} />
+        <Route path="/properties/:id" element={<PropertyDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
